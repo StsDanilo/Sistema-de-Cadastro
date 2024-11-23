dd409d040573fdf25943f19eb83e9d71e99045a1
Danilo Santos <StsDanilo@users.noreply.github.com>
2024-11-23 13:23:35 -0300

    Verificações básicas e registro implementado

---
+++ SistemaDeCadastroDeUsuarios/Entities/User.cs
@@ -0,0 +1,32 @@
+﻿using SistemaDeCadastroDeUsuarios.Services;
+
+namespace SistemaDeCadastroDeUsuarios.Entities
+{
+    internal class User
+    {
+        public string FirstName { get; private set; }
+        public string FullName { get; private set; }
+        public string Email { get; private set; }
+        public string Password { get; private set; }
+        public long Cpf { get; private set; }
+        public int Id { get; private set; }
+        public DateTime DateOfBirth { get; private set; }
+
+        public User(string firstName, string fullName, string email, string password, long cpf, DateTime dateOfBirth)
+        {
+            FirstName = firstName;
+            FullName = fullName;
+            Email = email;
+            Password = password;
+            Cpf = cpf;
+            DateOfBirth = dateOfBirth;
+            Id = DataProcessing.GenerateId();
+
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName};{FullName};{Email};{Password};{Cpf};{DateOfBirth.ToString("dd/MM/yyyy")};{Id}";
+        }
+    }
+}
---
+++ SistemaDeCadastroDeUsuarios/Program.cs
@@ -0,0 +1,30 @@
+﻿using SistemaDeCadastroDeUsuarios.Entities;
+using SistemaDeCadastroDeUsuarios.Services;
+
+namespace SistemaDeCadastroDeUsuarios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Deseja cadastrar, acessar ou excluir uma conta?");
+            string command = Console.ReadLine().ToLower();
+
+            switch(command)
+            {
+                case "cadastrar":
+                    Actions.Register();
+                    break;
+                case "acessar":
+
+                    break;
+                case "excluir:":
+
+                    break;
+                default:
+                    Console.WriteLine("Comando Inválido");
+                    break;
+            }
+        }
+    }
+}
